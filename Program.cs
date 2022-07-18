ab1e99366ee518a39ab75777866a7686e81b5463
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-19 00:35:41 +0200

    ok

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace T09_Ejercicio4
+{
+    class PruebaPersona
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona();
+            persona1.SetNombre("Pepe");
+            persona1.Saludar();
+            Persona persona2 = new Persona();
+            persona2.SetNombre("Lola");
+            persona2.Saludar();
+        }
+    }
+}
---
+++ persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace T09_Ejercicio4
+{
+    public class Persona
+    {
+        private string nombre;
+
+        public Persona()
+        {
+        }
+
+        public void SetNombre(string elnombre)
+        {
+            nombre = elnombre;
+        }
+
+        public void Saludar()
+        {
+            Console.WriteLine("Hola, soy " + nombre);
+        }
+    }
+}
